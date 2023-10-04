b6c62d3dd67300320b0dfb08081036a7dff6aca8
amanda vikdal <vikdal.amanda@gmail.com>
2023-10-04 19:08:50 -0400

    commit tree 5d73b8e4bf5dcae08dece62575c927180e04efeb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Student.create(first_name: "Luke", last_name: "Skywalker", grade: 8)
+# Student.create(first_name: "Obiwan", last_name: "Kinobe", grade: 3)
+# Student.create(first_name: "yoda", last_name: nil, grade: 53)
+
+
